6b0fa38c453e305a0aee927612aee3b574a1fbca
DaniloLima122 <limadanilo06@gmail.com>
2021-08-18 22:08:56 -0300

    setting up environment

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { MenuModule } from "@components/menu/menu.module";
+import { HomeComponent } from "./home.component";
+
+@NgModule({
+    declarations: [HomeComponent],
+    imports: [CommonModule, MenuModule],
+    exports: [HomeComponent]
+})
+export class HomeModule { }
